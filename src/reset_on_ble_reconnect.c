0c23ce673710d1a739e706f39f18e9878a0621d6
buwon <buwon@mai-world.com>
2026-08-24 07:06:56 +0900

    feat(config): BLE 재연결 시 키/마우스 상태와 레이어 초기화
    호스트와의 BLE 연결이 끊겼다가 재접속될 때, 연결이 끊긴 동안 release가
    전달되지 못해 마우스 클릭이나 키가 눌린 채로 고착되고 자동 마우스 레이어
    전환도 꼬이는 문제가 있었다. zmk_ble_active_profile_changed 이벤트로
    재연결 시점을 감지해서 기본 레이어로 복귀시키고, 키보드/컨슈머/마우스
    HID 상태를 비운 뒤 리포트를 강제로 재전송하도록 커스텀 리스너를 추가했다.

---
+++ src/reset_on_ble_reconnect.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2026 buwon
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+#if IS_ENABLED(CONFIG_ZMK_BLE)
+
+#include <zmk/ble.h>
+#include <zmk/endpoints.h>
+#include <zmk/event_manager.h>
+#include <zmk/events/ble_active_profile_changed.h>
+#include <zmk/keymap.h>
+
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+/* 호스트와의 활성 BLE 프로필이 끊겼다가 재연결되는 순간, 연결이 끊긴 동안
+ * release가 호스트로 전달되지 못해 눌린 채로 고착될 수 있는 키/클릭 상태와
+ * 자동 전환된 레이어를 강제로 초기화한다. */
+static int reset_state_on_ble_reconnect(const zmk_event_t *eh) {
+    if (!zmk_ble_active_profile_is_connected()) {
+        return ZMK_EV_EVENT_BUBBLE;
+    }
+
+    LOG_INF("Active BLE profile reconnected: resetting HID state and layers");
+
+    zmk_keymap_layer_to(zmk_keymap_layer_default(), false);
+    zmk_endpoints_clear_current();
+
+#if IS_ENABLED(CONFIG_ZMK_POINTING)
+    zmk_endpoints_send_mouse_report();
+#endif
+
+    return ZMK_EV_EVENT_BUBBLE;
+}
+
+ZMK_LISTENER(reset_state_on_ble_reconnect, reset_state_on_ble_reconnect);
+ZMK_SUBSCRIPTION(reset_state_on_ble_reconnect, zmk_ble_active_profile_changed);
+
+#endif // IS_ENABLED(CONFIG_ZMK_BLE)
